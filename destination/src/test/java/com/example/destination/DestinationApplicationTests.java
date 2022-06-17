2c432703bbd26851091ffef993efff4dcd14ed5d
casper.aangeenbrug <casper.aangeenbrug@omoda.nl>
2022-06-17 12:13:41 +0200

    Add Spring projects

---
+++ destination/src/test/java/com/example/destination/DestinationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.destination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DestinationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
